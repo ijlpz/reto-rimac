96b941865e09186701675d33d891e589e5f4b050
Jorge <ijlpz@hotmail.com>
2024-03-19 04:57:31 -0500

    :recycle: Select component refactor

--- src/components/common/Select/Select.tsx
+++ src/components/common/Select/Select.tsx
@@ -1,4 +1,9 @@
-import React, { ChangeEvent, CSSProperties, FunctionComponent, useState } from 'react';
+import React, {
+  ChangeEvent,
+  CSSProperties,
+  FunctionComponent,
+  useState,
+} from 'react';
 
 interface Option {
   value: string;
@@ -14,6 +19,8 @@ interface SelectProps {
   selectStyle?: CSSProperties;
   labelStyle?: CSSProperties;
   onChange?: (value: string) => void;
+  feedback?: string;
+  variant?: 'primary' | 'success' | 'error';
 }
 
 export const Select: FunctionComponent<SelectProps> = ({
@@ -23,8 +30,9 @@ export const Select: FunctionComponent<SelectProps> = ({
   disabled = false,
   className,
   selectStyle,
-  labelStyle,
   onChange,
+  feedback,
+  variant = 'primary',
 }) => {
   const [selectedValue, setSelectedValue] = useState(value || '');
 
@@ -37,43 +45,56 @@ export const Select: FunctionComponent<SelectProps> = ({
   };
 
   return (
-    <div className={`${className || ''} relative`}>
-      <select
-        id={name}
-        name={name}
-        value={selectedValue}
-        className={`
+    <div className={`${className || ''}`}>
+      <div className="relative">
+        <select
+          id={name}
+          name={name}
+          value={selectedValue}
+          className={`
           block w-full h-14 p-4 pr-12 bg-transparent text-base
           rounded-lg border
           ${disabled ? 'text-gray-300 border-gray-300' : 'border-gray-600'}
+          ${feedback ? `text-${variant} border-primary` : `border-gray-600`}
           focus:outline-none
           appearance-none
         `}
-        style={selectStyle}
-        disabled={disabled}
-        onChange={handleChange}
-      >
-        {options.map((option) => (
-          <option key={option.value} value={option.value}>
-            {option.label}
-          </option>
-        ))}
-      </select>
-      <div className="absolute inset-y-0 right-0 flex items-center px-2 pointer-events-none">
-        <svg
-          className={`h-6 w-6 text-gray-600 ${disabled ? 'text-gray-300' : ''}`}
-          xmlns="http://www.w3.org/2000/svg"
-          viewBox="0 0 20 20"
-          fill="currentColor"
-          aria-hidden="true"
+          style={selectStyle}
+          disabled={disabled}
+          onChange={handleChange}
         >
-          <path
-            fillRule="evenodd"
-            d="M5.293 7.293a1 1 0 011.414 0L10 10.586l3.293-3.293a1 1 0 111.414 1.414l-4 4a1 1 0 01-1.414 0l-4-4a1 1 0 010-1.414z"
-            clipRule="evenodd"
-          />
-        </svg>
+          {options.map((option) => (
+            <option key={option.value} value={option.value}>
+              {option.label}
+            </option>
+          ))}
+        </select>
+        <div className="absolute inset-y-0 right-0 flex items-center px-2 pointer-events-none">
+          <svg
+            className={`h-6 w-6 ${feedback ? 'text-primary' : 'text-gray-600'} ${
+              disabled ? 'text-gray-300' : ''
+            }`}
+            xmlns="http://www.w3.org/2000/svg"
+            viewBox="0 0 20 20"
+            fill="currentColor"
+            aria-hidden="true"
+          >
+            <path
+              fillRule="evenodd"
+              d="M5.293 7.293a1 1 0 011.414 0L10 10.586l3.293-3.293a1 1 0 111.414 1.414l-4 4a1 1 0 01-1.414 0l-4-4a1 1 0 010-1.414z"
+              clipRule="evenodd"
+            />
+          </svg>
+        </div>
       </div>
+
+      {feedback && (
+        <small
+          className={`block pl-4 pt-[3px] text-xs text-${variant} whitespace-pre`}
+        >
+          {feedback}
+        </small>
+      )}
     </div>
   );
 };
