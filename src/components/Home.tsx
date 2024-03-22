204c5ce9aa56ab1280a4592781f032d843159b31
Jorge <ijlpz@hotmail.com>
2024-03-22 14:05:18 -0500

    :lipstick: updare styles movil home

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -71,7 +71,7 @@ export const Home = () => {
       <div className="relative p-0 m-0 ">
         <BackgroundHome />
         <Header />
-        <div className="max-w-[73em] mx-auto px-6 pt-9 pb-16 relative z-20">
+        <div className="max-w-[73em] mx-auto px-6 pt-0 md:pt-9 pb-16 relative z-20">
           <div className="flex flex-col lg:flex-row lg:space-x-6 h-full">
             <div className="w-full lg:w-1/2 h-full hidden lg:flex">
               <div className="w-full h-full relative m-auto">
@@ -85,14 +85,14 @@ export const Home = () => {
                 />
               </div>
             </div>
-            <div className="max-w-[400px] m-auto px-6 lg:w-1/2 mt-6 lg:mt-0">
+            <div className="max-w-[350px] md:max-w-[400px] m-auto px-0 md:px-6 lg:w-1/2 mt-6 lg:mt-0">
               <div className="flex flex-row items-center justify-between">
                 <div className="mr-2">
                   <Tag
                     text="Seguro Salud Flexible"
-                    className="font-bold text-sm py-1 lg:py-0 lg:px-2 -tracking-tighter lg:text-sm"
+                    className="font-bold text-sm py-0 !px-1 lg:px-2 -tracking-tighter lg:text-sm"
                   />
-                  <h1 className="text-[32px] lg:text-[2.05em] mt-4 font-bold tracking-normal text-left leading-10">
+                  <h1 className="text-[1.7em] leading-7 lg:text-[2.05em] mt-4 font-bold tracking-normal text-left md:leading-10">
                     Creado para ti y tu familia
                   </h1>
                 </div>
