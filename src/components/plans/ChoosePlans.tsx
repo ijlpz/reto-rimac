1398f370a0f4d964fe536fb1b9b4c2859cc69524
Jorge <ijlpz@hotmail.com>
2024-03-22 11:47:33 -0500

    :iphone: test view cards 4

--- src/components/plans/ChoosePlans.tsx
+++ src/components/plans/ChoosePlans.tsx
@@ -13,18 +13,20 @@ interface ChoosePlansProps {
 
 const DISCOUNT_RATE = 5;
 
-export const ChoosePlans: FC<ChoosePlansProps> = ({ ageUser, selectedCard, user }) => {
-
-    const { data: plans } = useQuery(
-        ['plans'],
-        () => apiService.getPlans(),
-        {
-          enabled: true,
-          retry: false,
-        },
-      );
+export const ChoosePlans: FC<ChoosePlansProps> = ({
+  ageUser,
+  selectedCard,
+  user,
+}) => {
+  const { data: plans } = useQuery(['plans'], () => apiService.getPlans(), {
+    enabled: true,
+    retry: false,
+  });
   return (
     <div className="max-w-[928px] m-auto">
+      <p>{`PRUEBA ${ageUser} ' ' ${selectedCard} ' ' ${JSON.stringify(
+        user,
+      )}`}</p>
       <div className={`flex flex-wrap justify-center gap-8`}>
         {plans?.list
           .filter((plan: IPlan) => plan?.age <= ageUser)
@@ -34,7 +36,9 @@ export const ChoosePlans: FC<ChoosePlansProps> = ({ ageUser, selectedCard, user 
               description={plan?.description}
               name={plan?.name}
               price={plan?.price}
-              discountRate={selectedCard === SelectCardTypes.FOR_OTHER ? DISCOUNT_RATE : 0}
+              discountRate={
+                selectedCard === SelectCardTypes.FOR_OTHER ? DISCOUNT_RATE : 0
+              }
               user={user}
             />
           ))}
