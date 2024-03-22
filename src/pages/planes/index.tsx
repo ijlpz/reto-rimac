b5e6d3764fdb090dc6893f8e85d04316004fe7af
Jorge <ijlpz@hotmail.com>
2024-03-22 08:00:49 -0500

    :sparkles: add plans page

--- src/pages/planes/index.tsx
+++ src/pages/planes/index.tsx
@@ -1,7 +1,9 @@
 import { Plans } from '@/components/Plans';
+import useRedirect from '@/hooks/useRedirect';
 import React from 'react';
 
 const Planes = () => {
+  useRedirect();
   return <Plans />;
 };
 
