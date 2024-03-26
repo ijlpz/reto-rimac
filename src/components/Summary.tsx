26501965709c55b7537e34d17246ea1d445e3480
Jorge <ijlpz@hotmail.com>
2024-03-26 01:14:49 -0500

    :adhesive_bandage: suppressHydrationWarning delete

--- src/components/Summary.tsx
+++ src/components/Summary.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useMemo } from 'react';
+import React, { useEffect, useMemo, useState } from 'react';
 import Header from './layout/Header/Header';
 import { Card } from './common/Card/Card';
 import { Icon } from './common/Icon/Icon';
@@ -9,17 +9,19 @@ import { StepperRimac } from './StepperRimac';
 import { useRouter } from 'next/router';
 
 export const Summary = () => {
-  const summaryData = getUserInfo();
+  const [summaryDataObject, setSummaryDataObject] = useState<ISummary>(
+    {} as ISummary,
+  );
   const { push: NavigateTo } = useRouter();
-  const summaryDataObject: ISummary = useMemo(() => {
-    return summaryData ? JSON.parse(summaryData) : {};
-  }, [summaryData]);
 
   useEffect(() => {
-    if (!summaryDataObject.price) {
+    const summaryData = JSON.parse(getUserInfo() as string);
+
+    if (!summaryData.price) {
       NavigateTo('/planes');
     }
-  }, [NavigateTo, summaryDataObject.price]);
+    setSummaryDataObject(summaryData);
+  }, [NavigateTo]);
 
   return (
     <>
@@ -29,10 +31,7 @@ export const Summary = () => {
         <div className="sm:hidden">
           <Back route="/planes" className="text-[#4F4FFF] font-bold" />
         </div>
-        <StepperRimac
-          stepActive={2}
-          stepsText={['Planes y coberturas', 'Resumen']}
-        />
+        <StepperRimac stepActive={2} />
       </div>
 
       <div className="hidden sm:block">
@@ -45,30 +44,25 @@ export const Summary = () => {
         <h1 className="font-bold text-3xl sm:text-4xl text-[#141938]">
           Resumen del seguro
         </h1>
+
         <Card className="mt-12 mb-12">
           <Card.Header className="pb-4">
             <h2 className="text-xs font-bold">PRECIOS CALCULADOS PARA:</h2>
             <div className="flex flex-row items-center justify-items-center">
               <Icon name="UsersIcon" className="mr-2" size={24} />
-              <p suppressHydrationWarning className="text-xl font-bold">
-                {summaryDataObject.name + ' ' + summaryDataObject.lastName}
+              <p className="text-xl font-bold">
+                {summaryDataObject?.name + ' ' + summaryDataObject?.lastName}
               </p>
             </div>
           </Card.Header>
           <Card.Content>
             <p className="font-bold mt-4">Responsable de pago</p>
-            <p
-              suppressHydrationWarning
-            >{`DNI: ${summaryDataObject.documentNumber}`}</p>
-            <p
-              suppressHydrationWarning
-            >{`Celular: ${summaryDataObject.phone}`}</p>
+            <p>{`DNI: ${summaryDataObject?.documentNumber}`}</p>
+            <p>{`Celular: ${summaryDataObject?.phone}`}</p>
 
             <p className="font-bold mt-4">Plan elegido</p>
-            <p suppressHydrationWarning>{summaryDataObject.planName || ''}</p>
-            <p suppressHydrationWarning>{`Costo del Plan: $${
-              summaryDataObject.price || ''
-            } al mes`}</p>
+            <p>{summaryDataObject?.planName || ''}</p>
+            <p>{`Costo del Plan: $${summaryDataObject?.price || ''} al mes`}</p>
           </Card.Content>
         </Card>
       </div>
